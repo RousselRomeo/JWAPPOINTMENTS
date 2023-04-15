545bcce40c4443d67237ab219244dd8d761c087c
RousselRomeo <rousselrommeo@yahoo.com>
2023-04-16 00:30:29 +0200

    v1.0

--- server.js
+++ server.js
@@ -49,107 +49,24 @@ app.use(
 
 app.use(express.static("public"));
 //"mongodb+srv://procees.env.MUNGODB_USER:process.env.PASSWORD@cluster0.mxzx2.mongodb.net/userDB"
-//"mongodb://localhost:27017/userDB"
-mongoose.connect(
-  "mongodb+srv://roussel:tchatchou3991@cluster0.mxzx2.mongodb.net/userDB",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+//""
+mongoose.connect("mongodb://localhost:27017/FieldService", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "please specify name a name in the name field"],
   },
-  password: {
-    type: String,
-    required: [true, "please specify a password in the password field"],
-  },
-  playerName: String,
-  playerhighestLevel: { type: Number, default: 0 },
-  secretToken: String,
-  active: Boolean,
+  time: { type: String, default: "" },
 });
 
-const User = mongoose.model("User", userSchema);
-
-/*app.get("/video", function (req, res) {
-  const range = req.headers.range;
-  const videoPath = __dirname + "/lascars.mp4";
-  const videoSize = fs.statSync(videoPath).size;
-  console.log(range);
-  console.log(videoSize);
-
-  const chunkSize = 10 ** 6;
-
-  //replace: replaces all letters in range with an empty string
-  const start = Number(range.replace(/\D/g, ""));
-  console.log(start);
-  const end = Math.min(start + chunkSize, videoSize - 1);
-
-  console.log(end);
-  const contentLength = end - start + 1;
-  console.log(contentLength);
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-
-  //206:we are sending partial content
-  res.writeHead(206, headers);
-
-  const stream = fs.createReadStream(videoPath, { start, end });
-
-  stream.pipe(res);
-
-  /*console.log(req.headers);
-  res.writeHead(200, { "Content-Type": "video/mp4" });
-  var myReadStream = fs.createReadStream(__dirname + "/danceUB.mp4");
-  myReadStream.pipe(res);/
-});*/
+const User = mongoose.model("Appoinment", userSchema);
 
 app.get("/", function (req, res) {
-  User.find({})
-    .limit(20)
-    .sort({ playerhighestLevel: "descending" })
-    .exec(function (err, foundUsers) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("login", {
-          foundUsers: foundUsers,
-        });
-      }
-    });
-  //res.render("loginregister");
-});
-
-app.get("/startGame", function (req, res) {
-  res.sendFile(__dirname + "/public/game.html");
-});
-
-app.get("/verify/:secretToken", function (req, res) {
-  const secretToken = req.params.secretToken;
-  User.findOne({ secretToken: secretToken }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    }
-    if (!foundUser) {
-      req.flash("error4", "Email already verified Please! login");
-      res.redirect("/register");
-    } else {
-      //console.log(foundUser);
-      foundUser.active = true;
-      foundUser.secretToken = "";
-      foundUser.save();
-      req.flash("success2", "Email successfully Verified, You can now login");
-      res.redirect("/register");
-    }
-  });
+  res.sendFile(__dirname + "/public/login.html");
 });
 
 app.get("/logout", function (req, res) {
@@ -163,44 +80,6 @@ app.get("/logout", function (req, res) {
   });
 });
 
-// register player name
-app.post("/registerPlayerName", function (req, res) {
-  const userId = req.session.userId;
-  //console.log(userId);
-  const playerName = req.body.playerName;
-  console.log(playerName);
-  User.findOne({ _id: userId }, function (err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      User.find({})
-        .limit(20)
-        .sort({ playerhighestLevel: "descending" })
-        .exec(function (err, foundUsers) {
-          if (err) {
-            console.log(err);
-          } else {
-            foundUser.playerName = playerName;
-            foundUser.save();
-            res.render("welcomePage", {
-              playerName: foundUser.playerName,
-              playerhighestLevel: foundUser.playerhighestLevel,
-              foundUsers: foundUsers,
-            });
-          }
-        });
-      //console.log(foundUser);
-      // foundUser.playerName = playerName;
-      //foundUser.save();
-
-      /* res.render("welcomePage", {
-        playerName: foundUser.playerName,
-        playerhighestLevel: foundUser.playerhighestLevel,
-      });*/
-    }
-  });
-});
-
 //Register user
 
 app
@@ -208,12 +87,9 @@ app
     res.render("register");
   })
   .post("/register", function (req, res) {
+    console.log("rrrrrrrrrr");
     const newUser = new User({
       name: req.body.userName,
-      password: req.body.password,
-      playerName: "",
-      secretToken: randomstring.generate(),
-      active: false,
     });
 
     User.findOne({ name: req.body.userName }, function (err, foundUser) {
@@ -228,7 +104,7 @@ app
               "success",
               "Registration successful! Please verify your email"
             );
-            sendMail(newUser.name, newUser.secretToken);
+            //sendMail(newUser.name, newUser.secretToken);
             res.redirect("register");
           }
         });
@@ -248,7 +124,7 @@ app
 //user login
 app.post("/login", function (req, res) {
   const userName = req.body.userName;
-  const password = req.body.password;
+  //const password = req.body.password;
 
   User.findOne({ name: userName }, function (err, foundUser) {
     if (err) {
@@ -261,100 +137,90 @@ app.post("/login", function (req, res) {
       );
       res.redirect("/");
     } else {
-      if (foundUser.active === true) {
-        if (foundUser.password === password && foundUser.playerName === "") {
-          req.session.userId = foundUser._id;
-          res.render("playerName");
-          // res.render("welcomePage", { playerName: foundUser.playerName });
-        } else if (foundUser.password === password) {
-          req.session.userId = foundUser._id;
-          User.find({})
-            .limit(10)
-            .sort({ playerhighestLevel: "descending" })
-            .exec(function (err, foundUsers) {
-              if (err) {
-                console.log(err);
-              } else {
-                res.render("welcomePage", {
-                  playerName: foundUser.playerName,
-                  playerhighestLevel: foundUser.playerhighestLevel,
-                  foundUsers: foundUsers,
-                });
-              }
-            });
-        } else {
-          req.flash("incorrectPassword", "incorrect password,please Try again");
-          res.redirect("/");
-        }
-      } else {
-        req.flash("error3", "Username not verified! Please verify then login");
+      //(foundUser.password === password && foundUser.playerName === "")
+      //req.session.userId = foundUser._id;
+      //res.render("playerName");
+      // res.render("welcomePage", { playerName: foundUser.playerName });
 
-        res.redirect("/");
-      }
+      req.session.userId = foundUser._id;
+      User.find({})
+        .limit(10)
+        .exec(function (err, foundUsers) {
+          if (err) {
+            console.log(err);
+          } else {
+            //res.sendFile(__dirname + "/public/welcomePage.html");
+            /* res.render("welcomePage", {
+                playerhighestLevel: foundUser.time,
+                foundUsers: foundUsers,
+              });*/
+            res.send(foundUsers);
+          }
+        });
+
+      //req.flash("incorrectPassword", "incorrect password,please Try again");
+      //res.redirect("/");
     }
   });
 });
 
-app.post("/storeLevel", function (req, res) {
-  const { playerLevel } = JSON.parse(req.body.jsonStringPlayerLevel);
+app.post("/deleteAppointment", function (req, res) {
+  console.log("hello");
+  const { id } = req.body;
+  console.log(id);
+  const userId = req.session.userId;
+
+  User.findByIdAndDelete(id, function (err) {
+    if (err) console.log(err);
+    console.log("Successful deletion");
+  });
+
+  User.find({})
+    .limit(10)
+    .exec(function (err, foundUsers) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(foundUsers);
+      }
+    });
+});
+
+app.post("/addAppointment", function (req, res) {
+  const { time } = req.body;
+
+  // const userId = req.session.userId;
 
   const userId = req.session.userId;
   console.log(userId);
-  console.log(playerLevel);
   User.findOne({ _id: userId }, function (err, foundUser) {
+    console.log(foundUser.time);
+    if (foundUser.time === time) {
+      console.log("hello");
+      return;
+    } else {
+      foundUser.time = time;
+      foundUser.save();
+      User.find({})
+        .limit(20)
+        .exec(function (err, foundUsers) {
+          console.log(foundUsers);
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(foundUsers);
+          }
+        });
+    }
+
+    console.log(foundUser);
     if (err) {
       console.log(err);
     } else {
-      //console.log(foundUser);
-      if (foundUser.playerhighestLevel < playerLevel) {
-        foundUser.playerhighestLevel = playerLevel;
-        foundUser.save();
-      } else {
-      }
+      //console.log("hellllllo");
     }
   });
 });
-
-//send Mail to user
-
-const sendMail = function (email, secretToken) {
-  const Transport = nodemailer.createTransport({
-    service: "Yahoo",
-
-    auth: {
-      user: "rousseltchatchou@yahoo.com",
-      pass: "czwwnxrpnwhorljw",
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
-  });
-
-  var mailOptions = {
-    from: "rousseltchatchou@yahoo.com",
-    to: email,
-    subject: "Email Confirmation",
-    html: `Hi there,
-    <br/>
-    Thank you for registering!
-    <br/><br>
-    Eager to test how far you can memorise! and compete with friends!
-    <br/>
-    then click below to verify your account
-    <br/><br>
-      <a href="https://afternoon-retreat-41766.herokuapp.com/verify/${secretToken}"> <button style="color:green;font-size:40px;"> verify </button> </a> 
-    <br/><br>
-   Show  <b>Roussel Tchatchou</b> that your are the best !`,
-  };
-
-  Transport.sendMail(mailOptions, function (error, response) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("message sent");
-    }
-  });
-};
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server is listenning at port 3000");
